e67c41299363442fb7a78916a231a0cec81efca9
Althea Smith <althea.o.smith@gmail.com>
2015-12-03 15:56:33 -0500

    registered models in admin and changed Keywords and Articles to Keyword and Article

--- cmbs_proj/cmbs/admin.py
+++ cmbs_proj/cmbs/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from cmbs.models import Deal, Loan, Property, Lease, Keywords, Articles, Publisher
+from cmbs.models import Deal, Loan, Property, Lease, Keyword, Article, Publisher
 
 admin.site.register(Deal)
 admin.site.register(Loan)
 admin.site.register(Property)
 admin.site.register(Lease)
-admin.site.register(Keywords)
-admin.site.register(Articles)
-# admin.site.register(Keyword)
-# admin.site.register(Article)
+admin.site.register(Keyword)
+admin.site.register(Article)
+# admin.site.register(Keywords)
+# admin.site.register(Articles)
 admin.site.register(Publisher)
