1c5f0a7091a48f3678e1b23d03957fc31cc99eae
Althea Smith <althea.o.smith@gmail.com>
2015-12-04 19:45:21 -0500

    DRYed out JavaScript with function for returnign articles from every AJAX query

--- cmbs_proj/cmbs/static/cmbs/main.js
+++ cmbs_proj/cmbs/static/cmbs/main.js
@@ -1,7 +1,7 @@
 $(document).ready( function() {
-// Create function for returning articles since its the same in each one
-  $.get('/all', function(data){
-    console.log(data.articles)
+
+  // Function for returning articles with AJAX-returned data:
+  function post_articles(data) {
     $('#searchresults').empty()
     for (x in data.articles) {
       $('#searchresults').append(
@@ -10,58 +10,31 @@ $(document).ready( function() {
         <p>' + data.articles[x].blurb + '</p></br>'
       );
     }
+  }
+
+  // AJAX request for All Articles (default page)
+  $.get('/all', function(data){
+    post_articles(data);
   });
 
   // AJAX request for Deal Search
   $('#deal_search').click(function(event){
-    $search = $('#deal_search_term').val()
-    console.log('term', $('#deal_search_term').val())
-    console.log($search)
-    $.get('/deal_search/' + $search, function(data){
-      $('#searchresults').empty()
-      for (x in data.articles) {
-        $('#searchresults').append(
-          '<a href=' + data.articles[x].url + '>\
-          <h4>' + data.articles[x].title + '</h4></a>\
-          <p>' + data.articles[x].blurb + '</p></br>'
-        );
-      }
+    $.get('/deal_search/' + $('#deal_search_term').val(), function(data){
+      post_articles(data);
     });
   });
 
   // AJAX request for Loan Search
   $('#loan_search').click(function(event){
-    var $search = $('#loan_search_term').val()
-    console.log('term', $('#loan_search_term').val())
-    console.log($search)
-    var url = String('/loan_search/' + $search)
-    $.get(url, function(data){
-      $('#searchresults').empty()
-      for (x in data.articles) {
-        $('#searchresults').append(
-          '<a href=' + data.articles[x].url + '>\
-          <h4>' + data.articles[x].title + '</h4></a>\
-          <p>' + data.articles[x].blurb + '</p></br>'
-        );
-      }
+    $.get('/loan_search/' + $('#loan_search_term').val(), function(data){
+      post_articles(data);
     });
   });
 
   // AJAX request for Property Search
   $('#property_search').click(function(event){
-    var $search = $('#property_search_term').val()
-    console.log('term', $('#property_search_term').val())
-    console.log($search)
-    var url = String('/property_search/' + $search)
-    $.get(url, function(data){
-      $('#searchresults').empty()
-      for (x in data.articles) {
-        $('#searchresults').append(
-          '<a href=' + data.articles[x].url + '>\
-          <h4>' + data.articles[x].title + '</h4></a>\
-          <p>' + data.articles[x].blurb + '</p></br>'
-        );
-      }
+    $.get('/property_search/' + $('#property_search_term').val(), function(data){
+      post_articles(data);
     });
   });
 });
